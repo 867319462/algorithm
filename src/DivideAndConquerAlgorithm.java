4d7e92b7a904d07e881f821f1f6ed0261387797d
三石狰 <sdwxl1995@163.com>
2020-01-11 20:07:54 +0800

    Initial commit

---
+++ src/DivideAndConquerAlgorithm.java
@@ -0,0 +1,25 @@
+/**
+ * @ClassName DivideAndConquerAlgorithm
+ * @Description 分治算法
+ * @Author wangxl
+ * @Date 2020/1/6 22:10
+ **/
+public class DivideAndConquerAlgorithm {
+    public static void main(String[] args) {
+        hanoiTower(4, 'A', 'B', 'C');
+    }
+
+    public static void hanoiTower(int number, char a, char b, char c) {
+        if (number == 1) {
+            // 当一个盘子时从A->C
+            System.out.println("第" + number + "个盘子：从" + a + "到" + c);
+        } else {
+            // 当盘子不为一时
+            // 从A->B
+            hanoiTower(number - 1, a, c, b);
+            System.out.println("第" + number + "个盘子：从" + a + "到" + c);
+            // 从B->C
+            hanoiTower(number - 1, b, a, c);
+        }
+    }
+}
