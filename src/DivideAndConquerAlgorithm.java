45356122cfc3f3590fef939dd7d5ecba9fd46f24
三石狰 <sdwxl1995@163.com>
2020-01-15 22:38:39 +0800

    分治算法

--- src/DivideAndConquerAlgorithm.java
+++ src/DivideAndConquerAlgorithm.java
@@ -9,6 +9,14 @@ public class DivideAndConquerAlgorithm {
         hanoiTower(4, 'A', 'B', 'C');
     }
 
+    /**
+     * 汉诺塔游戏
+     *
+     * @param number 层数
+     * @param a      A盘
+     * @param b      B盘
+     * @param c      C盘
+     */
     public static void hanoiTower(int number, char a, char b, char c) {
         if (number == 1) {
             // 当一个盘子时从A->C
